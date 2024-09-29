73f74a83f389235f09be8a626027d5eb06c3df83
anddev <Ma1nmailAD@gmail.com>
2024-09-29 17:45:25 +0300

    Тут хотя бы что то работает

--- src/Window.java
+++ src/Window.java
@@ -1,51 +1,35 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.Graphics2D;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 import GraphicObjects.ComputerSystemUnit;
 
-public class Window extends JPanel implements ActionListener {
+public class Window extends JFrame {
 
     final static int FORM_WIDTH = 700;
     final static int FORM_HEIGHT = 700;
 
     private ComputerSystemUnit unit;
 
-    private Timer timer;
-
     Window(){
         setSize(FORM_WIDTH, FORM_HEIGHT);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);}
 
-
-        setPreferredSize(new Dimension(FORM_WIDTH, FORM_HEIGHT));
-
-        timer = new Timer(500, this);
-        timer.start();
-
-        setVisible(true);
+    public void animate(){
+        for (int i = 0; i < 20; i++){
+            this.unit.move(10);
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 
-//    public void animate(){
-//        for (int i = 0; i < 20; i++){
-//            this.unit.move(10);
-//            try {
-//                Thread.sleep(500);
-//            } catch (InterruptedException e) {
-//                throw new RuntimeException(e);
-//            }
-//        }
-//     }
+    public void paint(Graphics g) {
 
-    public void actionPerformed(ActionEvent e) {
-        this.unit.move(10);
-
-        //repaint();
-    }
-
-    public void paintComponent(Graphics g) {
-        super.paintComponents(g);
         Point anchor = new Point(240, 180);
         // преобразование графического объекта
         Graphics2D graph2d = (Graphics2D) g;
@@ -62,22 +46,12 @@ public class Window extends JPanel implements ActionListener {
         unit.setUnitWidth(220);
         unit.draw();
 
+
+
         graph2d.setFont(new Font("Arial", Font.BOLD, 24));
         graph2d.drawString("This is computer", anchor.x, anchor.y);
-    }
 
-    public static void main(String[] args) {
-
-        JFrame frame = new JFrame("AnimationExample");
-
-        Window window = new Window();
-
-        frame.add(window);
-
-        frame.pack();
-        frame.setLocationRelativeTo(null);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
+        animate();
     }
 
 }
